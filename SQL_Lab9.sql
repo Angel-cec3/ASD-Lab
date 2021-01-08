855c1a9e7d3c20f9e5fde36bcad8313906841605
Angel Mary Alex <44723357+Angel-cec3@users.noreply.github.com>
2021-01-08 16:26:30 +0530

    Expt - 9

---
+++ SQL_Lab9.sql
@@ -0,0 +1,29 @@
+CREATE TABLE Store (
+order_no INT,
+code VARCHAR(3),
+item_name CHAR(30),
+quantity INT,
+price INT,
+discount VARCHAR(4),
+mrp INT );
+
+INSERT INTO Store VALUES
+(121,'S2','Mug',10,250,'10%',300),
+(152,'s3','Jar',5,500,'50%',1000);
+
+SELECT * FROM Store;
+
+CREATE VIEW available AS
+SELECT item_name,quantity FROM Store;
+
+SELECT * FROM available;
+
+INSERT INTO Store VALUES
+(150,'F1','Jam',5,400,'10%',500);
+
+SELECT * FROM Store;
+
+SELECT * FROM available;
+
+DROP VIEW available;
+
